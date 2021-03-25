2249c1b590b057979d1e4cce065febe66d9ad6d8
Johnny William <50759662+Jownao@users.noreply.github.com>
2021-03-25 20:54:53 -0300

    ListaDuplamenteEncadeadaCircular

---
+++ ListaDuplamenteEncadeadaCircular/Main.java
@@ -0,0 +1,61 @@
+package ListaDuplamenteEncadeadaCircular;
+
+import Exercicio02_Anderson_ListaEncadeada.*;
+import ListaDuplamenteEncadeadaCircular.ListaDuplaCircular;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //Estanciando alunos
+    	Aluno a1 = new Aluno("Johnny", 40);
+        Aluno a2 = new Aluno("Paulo", 19);
+        Aluno a3 = new Aluno("Luiz", 19);
+        Aluno a4 = new Aluno("Gustavo", 20);
+        Aluno a5 = new Aluno("Vinicius", 19);
+        Aluno a6 = new Aluno("Augusto", 19);
+        Aluno a7 = new Aluno("Paul", 19);
+        
+        //Criando a Lista Encadeada
+        ListaDuplaCircular lista = new ListaDuplaCircular();
+        
+        
+        //Adicionando Alunos no inicio da lista
+        lista.adicionaInicio(a1);
+        lista.adicionaInicio(a2);
+        lista.adicionaInicio(a3);
+        lista.adicionaInicio(a3);
+        
+
+        
+        lista.removerDuplos();
+        
+        //Printando lista de Alunos
+        a1 = (Aluno) lista.Recupera(0);
+        a2 = (Aluno) lista.Recupera(1);
+        a3 = (Aluno) lista.Recupera(2);
+//      a4 = (Aluno) lista.Recupera(3);
+        
+           
+        System.out.println(a1.getNome());
+        System.out.println(a2.getNome());
+        System.out.println(a3.getNome());
+//      System.out.println(a4.getNome());
+           
+        System.out.println("-".repeat(100));
+        
+//        Removendo Aluno que esta no inicio
+//        lista.removeFim();
+//        
+//        //Printando lista de Alunos
+//        a1 = (Aluno) lista.Recupera(0);
+//        a2 = (Aluno) lista.Recupera(1);
+//        System.out.println(a1.getNome());
+//        System.out.println(a2.getNome());
+//        System.out.println("-".repeat(100));
+        
+        
+
+}
+}
+
+
